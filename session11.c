26f24089c968509b30c661857ffe9b5bcbf30e3f
Ali Saeed <alisaeed234.2002@gmail.com>
2022-08-31 08:45:25 -0700

    first commit

---
+++ session11.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void print_my_name()
+{
+printf("Ali Saeed\n");
+}
+
+void print_my_age()
+{
+printf("My age is : 20\n");
+}
+void print_my_faculty()
+{
+printf("Faculty : Engineering\n");
+}
+
+int main()
+{
+print_my_age();
+print_my_faculty();
+print_my_name();
+
+
+    return 0;
+}
